1dd929f7632866437d2e37dbc076f0589ef79f03
Ievgen Grabets <i.grabets@aimprosoft.com>
2015-10-29 13:00:13 +0200

    add pages with original player

--- routes/index.js
+++ routes/index.js
@@ -8,22 +8,33 @@ router.get('/', function(req, res, next) {
 
 /* GET video example page. async solution. */
 router.get('/video-example-async-solution', function(req, res, next) {
-  res.render('video-example.ejs');
+  res.render('video-example');
 });
 
 /* GET video example page with snippet. */
 router.get('/video-example-snippet-solution', function(req, res, next) {
-  res.render('snippet-solution.ejs');
+  res.render('snippet-solution');
 });
 
 /* GET video example page with snippet. */
 router.get('/index-with-snippet-solution', function(req, res, next) {
-  res.render('index-with-snippet-solution.ejs');
+  res.render('index-with-snippet-solution');
 });
 
 /* GET video example page with snippet. */
 router.get('/index-with-async-video', function(req, res, next) {
-  res.render('index-with-async-video.ejs');
+  res.render('index-with-async-video');
 });
+
+/* GET original index */
+router.get('/index-with-original-player', function(req, res, next) {
+  res.render('index');
+});
+
+/* GET original vied page index */
+router.get('/video-page-with-original-player', function(req, res, next) {
+  res.render('video-example-origin');
+});
+
 
 module.exports = router;
